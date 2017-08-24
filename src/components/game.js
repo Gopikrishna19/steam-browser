ef2ea44d5c1f0cc3e46185332d2327857d0c3380
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-08-23 20:45:54 -0500

    get installation statuses

--- src/components/game.js
+++ src/components/game.js
@@ -1,6 +1,17 @@
+import {execSync} from 'child_process';
 import PropTypes from 'prop-types';
 import React from 'react';
 import styles from './game.scss';
+
+const isInstalled = appId => {
+  try {
+    const installStatus = execSync(`reg query hkcu\\software\\valve\\steam\\apps\\${appId} /v Installed`)
+      .toString()
+      .replace(/\r\n/g, '')
+      .match(/0x(\d)/)[1];
+    return Boolean(parseInt(installStatus));
+  } catch (e) { return false; }
+};
 
 export const Game = props =>
   <li className={styles.game}>
@@ -10,6 +21,9 @@ export const Game = props =>
     <div className={styles.details}>
       <h2>{props.game.name}</h2>
       <p>Play time: {props.game.playtime_forever}</p>
+    </div>
+    <div className={styles.installStatus}>
+      {isInstalled(props.game.appid) ? 'Installed' : 'Not Installed'}
     </div>
   </li>;
 
