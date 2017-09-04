ba68df671f94b908e8693fa092203bb0ff9cebc0
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-09-04 14:04:20 -0500

    close menu after click and clean up code

--- src/components/app-menu.js
+++ src/components/app-menu.js
@@ -1,32 +1,67 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, {Component} from 'react';
 import styles from './app.scss';
 
-const showOnlyInstalled = function () {
-  this.toggleOnlyInstalled(!this.showOnlyInstalled);
+const closeAfter = (context, action) => () => {
+  action();
+  context.setState({open: false});
 };
 
-export const AppMenu = props =>
-  <div className={styles.appMenuContainer}>
-    <label className={styles.appMenuButton} htmlFor="app-menu">
-      <i className="material-icon">menu</i>
-    </label>
-    <input type="checkbox" id="app-menu"/>
-    <div className={styles.appMenu}>
-      <button className={props.showOnlyInstalled ? styles.toggled : ''} onClick={showOnlyInstalled.bind(props)}>
-        Show only installed
-      </button>
-      <button className={styles.sync} onClick={props.getGames}>Reload games from disk</button>
-      <button className={styles.sync} onClick={props.reloadGames}>Reload games from steam</button>
-      <button className={styles.clear} onClick={props.clearUserCredentials}>Clear credentials</button>
-    </div>
-  </div>;
+export class AppMenu extends Component {
+  constructor() {
+    super();
+
+    this.state = {open: false};
+
+    this.openMenu = this.openMenu.bind(this);
+  }
+
+  openMenu() {
+    this.setState({open: true});
+  }
+
+  render() {
+    return (
+      <div className={styles.appMenuContainer}>
+        <button className={styles.appMenuButton} onClick={this.openMenu}>
+          <i className="material-icon">menu</i>
+        </button>
+        <input type="checkbox" checked={this.state.open}/>
+        <div className={styles.appMenu}>
+          <button
+            className={this.props.showOnlyInstalled ? styles.toggled : ''}
+            onClick={closeAfter(this, () => this.props.toggleOnlyInstalled(!this.props.showOnlyInstalled))}
+          >
+            Show only installed
+          </button>
+          <button
+            className={styles.sync}
+            onClick={closeAfter(this, () => this.props.getGames())}
+          >
+            Reload games from disk
+          </button>
+          <button
+            className={styles.sync}
+            onClick={closeAfter(this, () => this.props.getGamesFromSteam())}
+          >
+            Reload games from steam
+          </button>
+          <button
+            className={styles.clear}
+            onClick={closeAfter(this, () => this.props.clearUserCredentials())}>
+            Clear credentials
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
 
 AppMenu.displayName = 'AppMenu';
 AppMenu.propTypes = {
   clearUserCredentials: PropTypes.func.isRequired,
-  reloadGames: PropTypes.func.isRequired,
   getGames: PropTypes.func.isRequired,
+  getGamesFromSteam: PropTypes.func.isRequired,
   showOnlyInstalled: PropTypes.bool.isRequired,
   toggleOnlyInstalled: PropTypes.func.isRequired
 };
--- src/connectors/menu.js
+++ src/connectors/menu.js
@@ -1,6 +1,7 @@
 import {connect} from 'react-redux';
 import {clearUserCredentials} from '../action-creators/clear-user-credentials';
 import {getGames} from '../action-creators/get-games';
+import {getGamesFromSteam} from '../action-creators/get-games-from-steam';
 import {toggleOnlyInstalled} from '../action-creators/toggle-only-installed';
 import {AppMenu} from '../components/app-menu';
 
@@ -11,7 +12,7 @@ export const Menu = connect(
   {
     getGames,
     clearUserCredentials,
-    reloadGames: () => {},
+    getGamesFromSteam,
     toggleOnlyInstalled
   }
 )(AppMenu);
